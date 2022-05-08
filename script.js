5cd4d6a558459a25467181b74c82e9ef2efe2134
Patrick Gebert <patrick.gebert@web.de>
2022-05-08 17:59:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ function loadPlaces(position) {
     };
 
     // CORS Proxy to avoid CORS problems
-    const corsProxy = 'https://cors-anywhere.herokuapp.com/';
+    // before: 'https://cors-anywhere.herokuapp.com/';
+    const corsProxy = 'https://corsanywhere.herokuapp.com/';
+    
+    
 
     // Foursquare API (limit param: number of maximum places to fetch)
     const endpoint = `${corsProxy}https://api.foursquare.com/v2/venues/search?intent=checkin
